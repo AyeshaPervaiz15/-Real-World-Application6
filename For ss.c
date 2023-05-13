2041c63c4183140fe420fef8f1cb752306693e92
Ayesha <ayeshapervaizlhr@gmail.com>
2023-05-13 20:02:12 +0500

    first commit

---
+++ For ss.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+    int main()
+    {
+        int n, i;
+        printf("Running the values");
+        printf("\nEnter a Number:  ");
+        scanf("%d",&n);
+        for (int (i) = 0; i < n; i++) {
+            printf("\n%d", i);
+        }
+
+    return 0;
+}
